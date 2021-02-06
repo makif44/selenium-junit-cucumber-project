b7b09a2ab40482187da91b9a56c358fa8185c368
Makif <makifkavukcu@gmail.com>
2021-02-06 18:47:57 +0200

    first commit

---
+++ src/test/java/com/cybertek/library/runners/CukesRunner.java
@@ -0,0 +1,22 @@
+package com.cybertek.library.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "json:target/cucumber.json",
+                "rerun:target/rerun.txt"
+        },
+
+        features = "src/test/resources/features",
+        glue = "com/cybertek/library/step_definitions",
+        dryRun = false,
+        tags = ""
+)
+public class CukesRunner {
+
+}
